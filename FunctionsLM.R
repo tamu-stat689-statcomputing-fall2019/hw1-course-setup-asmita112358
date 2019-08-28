d5adddd2fdc3a85e894e267439ef03b2a7e55eec
Asmita Roy <asmita112358@tamu.edu>
2019-08-28 13:43:47 -0500

    wrote mse function

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -22,7 +22,7 @@ calculateBeta <- function(X, Y){
 
 # Calculate MSE
 calculateMSE <- function(beta, beta_LS){
-  
-  # Return MSE - error ||beta - beta_LS||_2^2
+  MSE=sum((beta_LS-beta)^2)
+   #Return MSE - error ||beta - beta_LS||_2^2
   return(MSE)
 }
